30d643778739bb773dbe57ddfac97023cfc5fae6
embiem <mbeierling@gmail.com>
2025-11-24 17:21:10 +0100

    Add s3 video upload

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,144 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"mime"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	vid "github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/video"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Could not find video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithJSON(w, http.StatusUnauthorized, "Not owner")
+		return
+	}
+
+	fmt.Println("uploading video for video", videoID, "by user", userID)
+
+	err = r.ParseMultipartForm(1 << 30) // 1 GB max mem
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to parse form data", err)
+		return
+	}
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse video", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+
+	mediaType, _, _ := mime.ParseMediaType(contentType)
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Invalid video type", errors.New("only mp4 allowed"))
+		return
+	}
+
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create temp file", err)
+		return
+	}
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not write temp file", err)
+		return
+	}
+	tempFile.Seek(0, io.SeekStart)
+
+	aspectRatio, err := vid.GetVideoAspectRatio(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not get aspect ratio of video", err)
+		return
+	}
+
+	contentTypeParts := strings.Split(contentType, "/")
+	if len(contentTypeParts) == 0 {
+		respondWithJSON(w, http.StatusBadRequest, "Invalid content type")
+		return
+	}
+
+	// Create a random file path for the new file
+	bytes32 := make([]byte, 32)
+	_, err = rand.Read(bytes32)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error generating random file name", err)
+		return
+	}
+	randFileName := base64.RawURLEncoding.EncodeToString(bytes32)
+
+	prefix := "other"
+	switch aspectRatio {
+	case "16:9":
+		prefix = "landscape"
+	case "9:16":
+		prefix = "portrait"
+	}
+
+	fileKey := fmt.Sprintf("%s/%s.%s", prefix, randFileName, contentTypeParts[len(contentTypeParts)-1])
+
+	// Upload from temp file to S3
+	res, err := cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &fileKey,
+		Body:        tempFile,
+		ContentType: &mediaType,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error uploading video to S3", err)
+		return
+	}
+
+	slog.Info("S3 upload complete", "output", res)
+
+	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, fileKey)
+	video.VideoURL = &videoURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not save to DB", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
+}
